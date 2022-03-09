a73a9ac8c8625b83b9962ec6636b7ee6b8a9490d
ChuanSausage <91271418+ChuanSausage@users.noreply.github.com>
2022-03-09 22:08:16 +0800

    Update p1014.c

--- p1014.c
+++ p1014.c
@@ -12,17 +12,18 @@ int main(void)
     {
         sum_line+=line;
     }
-    printf("%ld",line-1);
-    long int loc=(line-1)-(sum_line-n);
+//这时上一个line符合条件后自加，而下一个line不符合，需要减一
+    printf("%ld",line-1);//确定在哪一行
+    long int loc=(line-1)-(sum_line-n);//确定在这一行中的哪个位置
     printf("\n%ld\n",loc);
     if(danshuang(line-1)==true)
-    printf("%ld/%ld",line-loc,loc);
+    printf("%ld/%ld",line-loc,loc);//找到所在元素和行数和位置数的关系
     else
     printf("%ld/%ld",loc,line-loc);
 
     return 0;
 }
-bool danshuang(long int linet)
+bool danshuang(long int linet)//bool值判断行数是奇数还是偶数，奇数和偶数的排列规则是不一样的
 {
     if ((linet%2)==0)
     return false;
